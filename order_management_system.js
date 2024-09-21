59362f1747692f8d6d75b6cc53e4d098be3f3257
Roan Ali <alir13@usf.edu>
2024-09-21 13:22:56 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,11 @@
+//Task 1: Creating an Inventory Array of Product Objects
+
+//Creating an Array of Coffee products in inventory
+const inventory = [
+    {name:"Matcha",price:4.25,quantity:20},
+    {name:"Spanish Latte",price:4.50,quantity:16},
+    {name:"Espresso",price:3,quantity:35},
+    {name:"Latte", price:4,quantity:65}
+];
+
+console.log ("Inventory:",(inventory)); //Logging the array 
